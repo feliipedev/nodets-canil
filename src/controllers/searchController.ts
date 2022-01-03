0475d2b5b43fd77136136b3e64ccbe5d87f40882
Felipe Monteiro <feliipemonteiro901@gmail.com>
2022-01-03 17:47:59 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
-export const search = (req: Request, res: Response) =>{
-    res.send('search')
-}
+export const search = (req: Request, res: Response) => {
+  let query: string = req.query.q as string;
+  let list = Pet.getFromName(query);
+  if (!query) {
+    res.redirect("/");
+    return;
+  }
+  res.render("pages/page", {
+    menu: createMenuObject(""),
+    list,
+    query,
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,14 @@ const server = express();
 
 server.set("view engine", "mustache");
 server.set("views", path.join(__dirname, "views"));
-server.set("mustache", mustache());
+server.engine("mustache", mustache());
 
 server.use(express.static(path.join(__dirname, "../public")));
 
 // Rotas
 server.use(mainRoutes);
 server.use((req,res) =>{
-    res.send('pagina não encontrada!')
+    res.render('pages/404')
 })
 
 server.listen(process.env.PORT);
